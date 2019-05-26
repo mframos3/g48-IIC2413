212c294c5293a2fa2b87183a513309157529a046
Benjamín Farías <bffarias@uc.cl>
2019-05-25 22:01:31 -0400

    :tada: Setup.

--- api/main.py
+++ api/main.py
@@ -1,14 +1,12 @@
 from flask import Flask, render_template, request, abort, json
 from pymongo import MongoClient
-import pandas as pd
-import matplotlib.pyplot as plt
 import os
 import atexit
 import subprocess
 
 MESSAGES_KEYS = ['message', 'sender', 'receptant', 'lat', 'long', 'date']
 
-mongod = subprocess.Popen('mongod', stdout=subprocess.DEVNULL)
+mongod = subprocess.Popen('mongod', stdout=subprocess.DEVNULL, shell=True)
 
 atexit.register(mongod.kill)
 
@@ -20,19 +18,23 @@ messages = db.messages
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
     return "<h1>API Homepage</h1>"
+
 
 @app.route("/messages")
 def get_messages():
     output = [msg for msg in messages.find({}, {"_id": 0})]
     return json.jsonify(output)
 
+
 @app.route("/messages/<int:mid>")
 def get_message(mid):
     msgs = list(messages.find({"mid": mid}, {"_id": 0}))
     return json.jsonify(msgs)
+
 
 @app.route("/messages", methods=['POST'])
 def create_message():
@@ -42,10 +44,10 @@ def create_message():
     count = messages.count_documents({})
     data["mid"] = count + 1
 
-    result = None # messages.insert_one(data)
-    #TODO: falta agregar todo el resto de atributos.
+    result = None  # messages.insert_one(data)
+    # TODO: falta agregar todo el resto de atributos.
 
-    if (result):
+    if result:
         message = "1 mensaje creado"
         success = True
     else:
@@ -53,6 +55,7 @@ def create_message():
         success = False
 
     return json.jsonify({'success': success, 'message': message})
+
 
 @app.route('/messages/<int:mid>', methods=['DELETE'])
 def delete_message(mid):
@@ -62,6 +65,7 @@ def delete_message(mid):
     message = f'Mensaje con id={mid} ha sido eliminado.'
 
     return json.jsonify({'result': 'success', 'message': message})
+
 
 if os.name == 'nt':
     app.run()
